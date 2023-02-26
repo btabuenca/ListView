d78e82a4d683dbba8e5a44fd39aa29a3788ccd99
btabuenca <bernardo.tabuenca@gmail.com>
2023-02-26 20:14:17 +0100

    Rebase version CUMN 2023

---
+++ app/src/main/java/es/upm/btb/listview/MainActivity.java
@@ -0,0 +1,57 @@
+package es.upm.btb.listview;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemClickListener {
+
+    // https://ibytecode.com/blog/custom-listview-with-image-and-text-using-with-arrayadapter/
+
+    public static final String[] names = new String[] { "Alonso Quijano",
+            "Dulcinea del Toboso", "Sancho Panza", "Rocinante", "Clavileño" };
+    public static final Integer[] images = { R.drawable.q1,
+            R.drawable.q2, R.drawable.q3, R.drawable.q4, R.drawable.q5 };
+
+    private ListView listView;
+    List<RowItem> rowItems;
+    private ArrayAdapter<RowItem> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rowItems = new ArrayList<RowItem>();
+        for (int i = 0; i < names.length; i++) {
+            RowItem item = new RowItem(names[i], images[i]);
+            rowItems.add(item);
+        }
+
+        listView = (ListView) findViewById(R.id.listView);
+        RowArrayAdapter adapter = new RowArrayAdapter(this,
+                R.layout.list_item, rowItems);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Item " + (position + 1) + ": " + rowItems.get(position),
+                Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+    }
+}
---
+++ app/src/main/java/es/upm/btb/listview/RowArrayAdapter.java
@@ -0,0 +1,48 @@
+package es.upm.btb.listview;
+
+import java.util.List;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RowArrayAdapter extends ArrayAdapter<RowItem> {
+    Context context;
+
+    public RowArrayAdapter(Context context, int resourceId, List<RowItem> items) {
+        super(context, resourceId, items);
+        this.context = context;
+    }
+
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtName;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        RowItem rowItem = getItem(position);
+
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, null);
+            holder = new ViewHolder();
+
+            holder.txtName = (TextView) convertView.findViewById(R.id.textView);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.imageView);
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder) convertView.getTag();
+
+
+        holder.txtName.setText(rowItem.getName());
+        holder.imageView.setImageResource(rowItem.getImageId());
+
+        return convertView;
+    }
+}
